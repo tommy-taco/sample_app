221f7630146f8ed00b6ee10de4a026d8b14398ee
AJ <ajhoward14@gmail.com>
2013-11-26 14:30:29 -0800

    Finished user rest actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
+	def index
+    	@users = User.paginate(page: params[:page])
+  	end
+	
 	def show
 		@user = User.find(params[:id])
 	end
-	
+  	
 	def new
 		@user = User.new
 	end
@@ -32,22 +37,35 @@ class UsersController < ApplicationController
 	      render 'edit'
 	    end
   	end
+  	
+  	def destroy
+	    User.find(params[:id]).destroy
+	    flash[:success] = "User deleted."
+	    redirect_to users_url
+  	end
 
   	private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
-
-    # Before filters
-
-    def signed_in_user
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
-    end
-
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
-    end
+	    def user_params
+	      params.require(:user).permit(:name, :email, :password,
+	                                   :password_confirmation)
+	    end
+	
+	    # Before filters
+	
+	    def signed_in_user
+	      unless signed_in?
+	        store_location
+	        redirect_to signin_url, notice: "Please sign in."
+	      end
+	    end
+	
+	    def correct_user
+	      @user = User.find(params[:id])
+	      redirect_to(root_url) unless current_user?(@user)
+	    end
+	    
+	    def admin_user
+      		redirect_to(root_url) unless current_user.admin?
+   		end
 end
