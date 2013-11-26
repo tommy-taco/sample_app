b93672ab56f4577df46444c6edf22ad607374b27
AJ <ajhoward14@gmail.com>
2013-11-26 12:41:49 -0800

    gettin there

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,37 +4,38 @@ class UsersController < ApplicationController
 	end
 	
 	def new
-	end
-	
-	def new
 		@user = User.new
 	end
+
+	def create
+	    @user = User.new(user_params)
+	    if @user.save
+	      sign_in @user
+	      flash[:success] = "Welcome to the Sample App!"
+	      redirect_to @user
+	    else
+	      render 'new'
+	    end
+	end
 	
-	 def create
-    @user = User.new(user_params)
-    if @user.save
-      # Handle a successful save.
-    else
-      render 'new'
-    end
-  end
+	def edit
+		@user = User.find(params[:id])
+	end
+	
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(user_params)
+		  # Handle a successful update.
+		else
+		  render 'edit'
+		end
+	end
 
-def create
-    @user = User.new(user_params)
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
-    else
-      render 'new'
-    end
-  end
+  	private
 
-  private
-
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
+	    def user_params
+	      params.require(:user).permit(:name, :email, :password,
+	                                   :password_confirmation)
+	    end
     
 end
